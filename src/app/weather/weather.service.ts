902e8c17326942eb5a112cc7a17117a93e55cc49
lavi09 <42918272+lavi09@users.noreply.github.com>
2020-05-10 19:27:18 -0700

    weather service and test

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,7 +1,8 @@
- import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ICurrentWeather } from '../icurrent-weather';
 import { environment } from 'src/environments/environment';
+import {map} from 'rxjs/operators';
 
 interface ICurrentWeatherData{
   weather: [
@@ -28,7 +29,22 @@ export class WeatherService {
   constructor(private httpClient:HttpClient) { }
 
   getCurrentWeather(city: string, country: string){
-    return this.httpClient.get<ICurrentWeatherData>
-     `${environment.baseUrl}`api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}
+     return this.httpClient.get<ICurrentWeatherData>(
+     `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${environment.appId}`
+    ).pipe(map(data => this.transformToICurrentWeather(data)))
     }
+  transformToICurrentWeather(data:ICurrentWeatherData): ICurrentWeather{
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: new Date (data.dt * 1000),
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+      temperature: this.convertKelvinToFahrenheit(data.main.temp),
+      description: data.weather[0].description
+    }
+  }
+
+  convertKelvinToFahrenheit(kelvin: number): number{
+    return kelvin * 9 / 5 - 459.67
+  }
 }
